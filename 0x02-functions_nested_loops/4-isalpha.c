2a5e79f7274a2b5f6d1b0ddd32253224cfc105a1
wikki2000 <wisdomokposin@gmail.com>
2023-09-13 13:18:10 -0700

    Resolve bug

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,11 +7,10 @@
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return 1; // Character is alphabetic
-	}
+
+		return (1);
+
 	else
-	{
-		return 0; // Character is not alphabetic
-	}
+
+		return (0);
 }
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_sign - Print the that prints the sign of a number
+ * @n: Stores the number
+ *
+ * Return: Return (0) if n > 0.
+ * Return (1) if n = 0.
+ * Return (-1) if n < 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(' ');
+		return (1);
+		_putchar(10);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(' ');
+		_putchar(10);
+		return (0);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(' ');
+		_putchar(10);
+		return (-1);
+	}
+
+	_putchar(10);
+}
