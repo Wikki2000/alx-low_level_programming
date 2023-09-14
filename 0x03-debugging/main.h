300a5062ef6292f00a288c216798d785dc663b1d
wikki2000 <wisdomokposin@gmail.com>
2023-09-14 09:33:11 -0700

    continue task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -12,8 +12,26 @@
  * This function takes three integers as input and returns the largest
  * of the three.
  *
- * * Return: The largest of the three integers.
+ * Return: The largest of the three integers.
  */
 int largest_number(int a, int b, int c);
 
+/**
+ * largest_number - Calculate numbbers of days remaining to make a year
+ * @month: The current month in a year
+ * @day: The current day in a year
+ * @year: The present year
+ *
+ * Return: The largest number
+ */
+void print_remaining_days(int month, int day, int year);
+
+/**
+ * convert_day - Convert any given month to day
+ * @month: The present month
+ * @day: The current day
+ *
+ * Return: The number of day
+ */
+int convert_day(int month, int day);
 #endif
