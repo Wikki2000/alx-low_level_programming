24e93403a621bffa0c0849a0af7eb92273797c6a
wikki2000 <wisdomokposin@gmail.com>
2023-09-14 08:49:54 -0700

    bug resolve

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,17 @@
 
 #include <stdio.h>
 
+/**
+ * largest_number - Find the largest number among three integers.
+ * @a: The first integer
+ * @b: The second integer
+ * @c: The third integer
+ *
+ * This function takes three integers as input and returns the largest
+ * of the three.
+ *
+ * * Return: The largest of the three integers.
+ */
 int largest_number(int a, int b, int c);
-void positive_or_negative(int i);
 
-#endif 
+#endif
