5de62b209bddeafdd5e109cf9926d7ecb27dec88
wikki2000 <wisdomokposin@gmail.com>
2023-09-15 12:46:28 -0700

    Add file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,12 +8,15 @@ void print_square(int size)
 {
 	int i;
 
+	if (size <= 0)
+		_putchar('\n');
+
 	for (i = 1; i <= size; i++)
 	{
 		int j;
 
 		for (j = 1; j <= size; j++)
 			_putchar('#');
-		_putchar(10);
+		_putchar('\n');
 	}
 }
