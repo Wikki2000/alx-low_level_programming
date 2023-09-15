ac2f4e24506b5e759460bb9dd74c9468e4a10f3c
wikki2000 <wisdomokposin@gmail.com>
2023-09-15 11:14:20 -0700

    Add file

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -8,8 +8,9 @@
 int main(void)
 {
 	print_diagonal(0);
-	print_diagonal(2);
-	print_diagonal(10);
-	print_diagonal(-4);
+	print_diagonal(-10);
+	print_diagonal(3);
+	print_diagonal(9);
+	print_diagonal(98);
 	return (0);
 }
