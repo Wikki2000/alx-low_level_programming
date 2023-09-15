19b9c97e626193540fb465fd1d87c4deac5b9d4f
wikki2000 <wisdomokposin@gmail.com>
2023-09-15 04:45:49 -0700

    Add file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print natural numbers from 0-9
+ */
+void more_numbers(void)
+{
+	int count = 1;
+
+	while (count < 11)
+	{
+		int num = 0;
+
+		while (num < 15)
+		{
+			_putchar(num + '0');
+			num++;
+		}
+		
+		_putchar(10);
+		count++;
+	}
+}
