ea75ee09c1351acaa2000fb3fd2617fc49e4bad4
wikki2000 <wisdomokposin@gmail.com>
2023-09-15 02:24:14 -0700

    Documented to betty standard

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,16 +4,18 @@
  * _islower - Checks if a character is lowercase.
  * @c: The character to be checked.
  *
+ * This function print alphabet in lower case.
+ *
  * Return: 1 if the character is lowercase, 0 otherwise.
  */
 int _isupper(int c)
 {
-        if (c >= 'A' && c <= 'Z')
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
