362245a180b3855610d71418c216599f0e3ae7ed
wikki2000 <wisdomokposin@gmail.com>
2023-09-15 03:15:49 -0700

    Add file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two given numbers
+ * @a: First number to be multiply
+ * @b: Second number to be multiply
+ *
+ * This function give the the product between any two given number
+ *
+ * Return: The product of the two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
