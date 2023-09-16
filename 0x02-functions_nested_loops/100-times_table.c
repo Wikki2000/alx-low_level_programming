ab4312df49ebbe3ad5cabcc8f5c448546e52bcbf
wikki2000 <wisdomokposin@gmail.com>
2023-09-16 14:03:02 -0700

    Add file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * void print_times_table -  prints the n times table, starting with 0
+ * print_times_table -  prints the n times table, starting with 0
  * @n: The array of time table to print
  */
 void print_times_table(int n)
