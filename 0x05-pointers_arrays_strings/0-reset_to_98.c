5698899e4d23926233713bf5789a0761f9d191be
wikki2000 <wisdomokposin@gmail.com>
2023-09-18 10:42:45 -0700

    Add file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,9 @@
 /**
  * reset_to_98 - Reset any given integer to 98
  * @n: The integer to be reset to 98
- *  
+ *
  * This function reset any given integer to 98
- * 
+ *
  * Return: The integer 98
  */
 void reset_to_98(int *n)
