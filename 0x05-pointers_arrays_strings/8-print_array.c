463d8a09b980776acc9c9390d8bd97cdd661ea61
wikki2000 <wisdomokposin@gmail.com>
2023-09-19 17:10:02 -0700

    Add file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Print any gien number of integer
+ *
+ * @a: pointer to the integer
+ * @n: Number of integer to be print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n <= 0)
+		printf("Error");
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+			putchar(',');
+		printf("%d", *a);
+		a++;
+	}
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,4 +13,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 void puts2(char *str);
 void rev_string(char *s);
+void print_array(int *a, int n);
+
 #endif
