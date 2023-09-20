b8e8d7d9bbb102e28c229ca94ec0d244c8937677
wikki2000 <wisdomokposin@gmail.com>
2023-09-19 21:29:20 -0700

    Add file

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+/**
+ * main - Generates a random valid password
+ *
+ * This program generates a random password based on the given character set and
+ * length. It uses the srand, rand, and time functions to ensure randomness.
+ *
+ * Return: Always 0.
+ */
+int main() {
+	/* Seed the random number generator with the current time */
+	srand(time(NULL));
+
+	/* Define the character set for the password */
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	/* Generate and print a random password */
+	printf("Random Password: ");
+	for (int i = 0; i < PASSWORD_LENGTH; i++) {
+		int index = rand() % (sizeof(charset) - 1);
+		putchar(charset[index]);
+	}
+	printf("\n");
+
+	return 0;
+}
+
