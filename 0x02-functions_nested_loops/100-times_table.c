5b3840cfbeaa00178c890c0ed5579ee568c70eeb
wikki2000 <wisdomokposin@gmail.com>
2023-09-20 20:54:11 -0700

    Add file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,6 @@ void print_times_table(int n)
 
 	if (n > 0 &&  n < 15)
 	{
-
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
@@ -21,7 +20,7 @@ void print_times_table(int n)
 
 				printf("%3d", ans);
 
-				if (j != n)
+				if (j >= 0 && j < n)
 				{
 					putchar(',');
 					putchar(' ');
@@ -31,4 +30,6 @@ void print_times_table(int n)
 			putchar('\n');
 		}
 	}
+		else
+			putchar('\n');
 }
