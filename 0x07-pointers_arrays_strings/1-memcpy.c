2f49bd97b9ad96044698b432441ada11a086786c
wikki2000 <wisdomokposin@gmail.com>
2023-09-25 16:18:29 -0700

    Add file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,6 @@
  * @src: Pointer to the source memory
  * @n: Number of bytes to copy
  *
- * Description:
  * This function copies 'n' bytes from the memory area pointed to by 'src' to
  * the memory area pointed to by 'dest'. It operates on a byte-by-byte basis.
  *
