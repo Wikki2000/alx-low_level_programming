875bcbfb52185c465416214d1b5226214b95c885
wikki2000 <wisdomokposin@gmail.com>
2023-09-25 19:32:31 -0700

    Add file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,12 @@
  * string pointed to by 's'. It returns a pointer to the first occurrence of
  * 'c' in the string, or NULL if 'c' is not found.
  *
- * Return: A pointer to the first occurrence of 'c' in 's', or NULL if not found.
+ * Return: Adress of pointer s, or NULL if not found.
  */
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
-	{	
+	{
 		if (*s == c)
 			return (s);
 		s++;
