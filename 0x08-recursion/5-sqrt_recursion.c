9798cdc92ae92642ce864da1680135beba626cd1
wikki2000 <wisdomokposin@gmail.com>
2023-09-29 17:03:26 -0700

    Add file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,11 +3,11 @@
 int actual_sqrt_recursion(int n, int i);
 
 /**
- *  * _sqrt_recursion - returns the natural square root of a number
- *   * @n: number to calculate the square root of
- *    *
- *     * Return: the resulting square root
- *      */
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root
+ *
+ * Return: the resulting square root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -16,13 +16,13 @@ int _sqrt_recursion(int n)
 }
 
 /**
- *  * actual_sqrt_recursion - recurses to find the natural
- *   * square root of a number
- *    * @n: number to calculate the sqaure root of
- *     * @i: iterator
- *      *
- *       * Return: the resulting square root
- *        */
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
