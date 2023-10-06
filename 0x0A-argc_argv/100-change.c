2b80f28eec9688b61a0a93d6009b75443d428ea0
root <wikki2000>
2023-10-06 04:59:23 -0700

    Resolve error

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,37 +7,37 @@
  * @argc: The number of command-line entered
  * @argv: The array of arguement entered as string
  *
- * Return: 
-*/
-int main(int argc, char *argv[])    
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
 {
-        int cent;
-        int mncoin = 0;
+	int cent;
+	int mncoin = 0;
 
-        if (argc != 2)
-                puts("Error");      
+	if (argc != 2)
+		puts("Error");
 
-        cent = atoi(argv[1]);       
+	cent = atoi(argv[1]);
 
-        while (cent > 0)
-        {
-                if (cent >= 25)     
-                        cent -= 25; 
+	while (cent > 0)
+	{
+		if (cent >= 25)
+			cent -= 25;
 
-                else if (cent >= 10)
-                        cent -= 10; 
+		else if (cent >= 10)
+			cent -= 10;
 
-                else if (cent >= 5)
-                        cent -= 5;
+		else if (cent >= 5)
+			cent -= 5;
 
-                else if (cent >= 2)
-                        cent -= 2;
+		else if (cent >= 2)
+			cent -= 2;
 
-                else if (cent >= 1)
-                        cent -= 1;
+		else if (cent >= 1)
+			cent -= 1;
 
-                mncoin++;
-        }
-        printf("%d\n", mncoin);
-        return (0);
+		mncoin++;
+	}
+	printf("%d\n", mncoin);
+	return (0);
 }
