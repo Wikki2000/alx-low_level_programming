6d82185e4395a1208d296937e0dce1005207b108
root <wikki2000>
2023-10-07 16:02:35 -0700

    My first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The size of memory to be allocated
+ *
+ * Return: Pointer to the allocated memory
+ *              on failure return NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Error! Memory allocation failed\n");
+		exit(98);
+	}
+	return (ptr);
+}
