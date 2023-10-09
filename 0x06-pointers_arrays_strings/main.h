17760f4d95a5fff508546a962c3516549e7fcf53
root <wikki2000>
2023-10-09 08:43:41 -0700

    Delete file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,4 @@ int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *rot13(char *);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
