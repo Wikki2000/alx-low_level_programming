95d8aa89273d49baf9d34baa20cb0c6a11b31fef
root <wikki2000>
2023-10-10 18:59:47 -0700

    Add filr

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,11 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = (struct dog *) malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
-	d->name;
-	d->age;
-	d->owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
 
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,17 @@
  * @age: Age of the dog
  * @owner: Name of dog owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
