6f3c6221ea08158660713491a975a76f9836714b
root <wikki2000>
2023-10-10 18:00:40 -0700

    My first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Store information of a dog
+ * @name: Pointer to name of dog
+ * @age: Age of the dog
+ * @owner: Name of dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
