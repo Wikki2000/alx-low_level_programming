bf2ea9f7c36afe30ece7a681bcb1b3944684e280
root <wikki2000>
2023-10-12 17:47:46 -0700

    My first commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Calculate the sum of all parameter entered
+ * @n: The number of parameter entered
+ * @..: The variable numbers to be added
+ *
+ * Return: Sum of all parameter entered by user
+ *      eslse return (0) if n = 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum = 0;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/2-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Print all the variable arguement integers
+ * @separator: The string to be printed between numbers
+ * @..: A variable numbers of number to be printed
+ *
+ * Return: Void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(args, int);
+		printf("%d", value);
+
+		/* Ensure that seperator not printed after number */
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
