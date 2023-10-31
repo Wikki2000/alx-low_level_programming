b4049c9ad41aab7d7f3f0af3fc076e2c487b61cb
wisdomokposin@gmail.com <wikki2000>
2023-10-30 23:29:08 -0700

    Resolve error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,10 +19,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[text_len] != '\0')
-		text_len++;
+	if (text_content != NULL)
+	{
+		while (text_content[text_len] != '\0')
+			text_len++;
 
-	bytes_written = write(fd, text_content, text_len);
+		bytes_written = write(fd, text_content, text_len);
+	}
 
 	if (bytes_written == -1)
 	{
