950fcc253af1d9083db1cc5509749f0e7285f19e
wisdomokposin@gmail.com <wikki2000>
2023-10-30 21:01:25 -0700

    Add file  2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * append_text_to_file - Append text to the end of a file.
  * @filename: Name of the file to append to.
@@ -8,10 +7,10 @@
  *
  * Return: 1 on success, -1 on failure.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t bytes_written, text_len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,24 +19,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	while (text_content[text_len] != '\0')
+		text_len++;
+
+	bytes_written = write(fd, text_content, text_len);
+
+	if (bytes_written == -1)
 	{
-		ssize_t text_len_Inc = 0;
-		ssize_t char_written;
-
-		while (text_content[text_len_Inc] != '\0')
-		{
-			text_len_Inc++;
-		}
-
-		char_written = write(fd, text_content, text_len_Inc);
-		if (char_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
 	return (1);
 }
+
