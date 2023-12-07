05f3e8cc7939604bee74890cba3488eddbe8ff9f
wikki2000 <wisdomokposin@gmail.com>
2023-12-07 04:12:07 -0800

    Add file  1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Count the number of element in the list
+ * @h: The head pointer to the list
+ *
+ * Return: The number of element in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	int count = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
