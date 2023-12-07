fa07911e9b570d5dfd1fb9e452109b6fb89451f1
wikki2000 <wisdomokposin@gmail.com>
2023-12-07 06:52:30 -0800

    Resolve bug in 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,26 +1,32 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint_end - add node to the end of a doubly lineked list
+ * @head: pointer to pointer that point to head node
+ * @n: data to store in the new node
+ * Return: address of the new node or NULL if failure occur
+ */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *temp = NULL, *new = NULL;
+	dlistint_t *new, *last;
 
-	new = malloc(sizeof(dlistint_t));
+	new = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	if (*head == NULL)
 	{
-		new->next = *head;
 		new->prev = NULL;
+		new->next = *head;
 		*head = new;
 		return (new);
 	}
-
-	temp = *head;
-	while (temp->next != NULL)
-		temp = temp->next;
-	new->prev = temp;
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	new->prev = last;
 	new->next = NULL;
-	temp->next = new;
-	return (NULL);
+	last->next = new;
+	return (new);
 }
