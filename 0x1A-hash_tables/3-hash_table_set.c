f0898dee5af96c6a977faeb380eea22e3f96cd36
wikki2000 <wisdomokposin@gmail.com>
2023-12-25 02:34:10 -0800

    Add file 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: pointer to the hash table
@@ -6,40 +7,39 @@
  * @value: value
  * Return: 1 - success, 0 - failure
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value) 
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *temp, *head, *node;
+	const char *new_value;
 	unsigned long int index;
+	hash_node_t *node, *head, *tmp;
 
-	if (ht == NULL || key == NULL || strcmp(value, "") == 0)
+	if (ht == NULL || !key)
 		return (0);
-
+	new_value = value == NULL ? "" : value;
+	node = create_hash_node(key, new_value);
+	if (node == NULL)
+		return (0);
+	/* create an array index using the hash_djb2 algorithm */
 	index = key_index((const unsigned char *)key, ht->size);
-	node = create_hash_node(key, value);
-	head = ht->array[index]; /* Pass the address of the first node */
-
-	/* Check if the generated index is empty. I.e no collision. */   
+	/* check if the node already exit in the table */
+	head = ht->array[index];
 	if (head == NULL)
 	{
 		ht->array[index] = node;
 		return (1);
 	}
-
-	temp = head; /* Initialize temp with head */
-
-	/* Update value if key exists */
-	while (temp)
+	tmp = head;
+	while (tmp)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			strcpy(temp->value, value);
+			strcpy(tmp->value, value);
 			return (1);
 		}
-		temp = temp->next;
+		tmp = tmp->next;
 	}
-
-	/* Handle collision */
-	node->next = head; /* Link head with new node (node->head) */
+	node->next = head;
 	ht->array[index] = node;
 	return (1);
 }
@@ -50,24 +50,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * @value: value of the node
  * Return: key/value pair structued memory
  */
+
 hash_node_t *create_hash_node(const char *key, const char *value)
 {
 	hash_node_t *node;
 	char *tmp1, *tmp2;
 
-	node = (hash_node_t *)malloc(sizeof(hash_node_t));       
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (node == NULL)
-		return NULL;
-
-	tmp1 = malloc(sizeof(char) * (strlen(key) + 1));  
+		return (NULL);
+	tmp1 = malloc(sizeof(char) * (strlen(key) + 1));
 	strcpy(tmp1, key);
-
+	tmp1[strlen(key)] = '\0';
+	node->key = tmp1;
 	tmp2 = malloc(sizeof(char) * (strlen(value) + 1));
 	strcpy(tmp2, value);
-
-	node->key = tmp1;
+	tmp2[strlen(value)] = '\0';
 	node->value = tmp2;
 	node->next = NULL;
-
-	return node;
+	return (node);
 }
