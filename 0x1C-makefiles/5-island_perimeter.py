a7098e204b38aad9c268f2ce01e486ed5b4a424a
wikki2000 <wisdomokposin@gmail.com>
2024-04-10 07:27:17 -0700

    Add file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Defines a function that calculates the perimeter of an island
+"""
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in grid
+
+    Args:
+        grid: list of list of integers representing the island
+
+    Returns:
+        Perimeter of the island
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
